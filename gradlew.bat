e664a210d13bb264d7dc52d8760696b1cdee4cef
Alejandra <alejandra.zuletag1@udea.edu.co>
2020-09-07 12:29:56 -0500

    Initial commit

---
+++ gradlew.bat
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="AndroidLayouts">
+    <shared>
+      <config />
+    </shared>
+  </component>
+  <component name="AndroidLogFilters">
+    <option name="TOOL_WINDOW_CUSTOM_FILTER" value="Open" />
+    <option name="TOOL_WINDOW_CONFIGURED_FILTER" value="Show only selected application" />
+  </component>
+  <component name="ChangeListManager">
+    <list default="true" id="dbcc62eb-2ec0-4b01-be2b-bb35649a3e6f" name="Default Changelist" comment="">
+      <change afterPath="$PROJECT_DIR$/app/src/main/java/com/alejandrazuleta/municipioscolombia/ui/NewVecinoActivity.kt" afterDir="false" />
+      <change afterPath="$PROJECT_DIR$/app/src/main/res/layout/activity_new_vecino.xml" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/build.gradle" beforeDir="false" afterPath="$PROJECT_DIR$/app/build.gradle" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/AndroidManifest.xml" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/AndroidManifest.xml" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/java/com/alejandrazuleta/municipioscolombia/ui/CrearFragment.kt" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/java/com/alejandrazuleta/municipioscolombia/ui/CrearFragment.kt" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/java/com/alejandrazuleta/municipioscolombia/ui/EliminarFragment.kt" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/java/com/alejandrazuleta/municipioscolombia/ui/EliminarFragment.kt" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/java/com/alejandrazuleta/municipioscolombia/ui/ListaFragment.kt" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/java/com/alejandrazuleta/municipioscolombia/ui/ListaFragment.kt" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/res/layout/fragment_crear.xml" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/res/layout/fragment_crear.xml" afterDir="false" />
+      <change beforePath="$PROJECT_DIR$/app/src/main/res/layout/fragment_eliminar.xml" beforeDir="false" afterPath="$PROJECT_DIR$/app/src/main/res/layout/fragment_eliminar.xml" afterDir="false" />
+      <change beforePath
